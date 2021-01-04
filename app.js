908ba34ff134035f677f60be3af1d1a44ae8648c
arronrees <64908445+arronrees@users.noreply.github.com>
2021-01-04 20:00:27 +0000

    Changed to display actual player name when won

--- app.js
+++ app.js
@@ -109,18 +109,18 @@ function displayResult(playerHand, computerHand) {
   } else if (playerHand === 'rock' && computerHand === 'paper') {
     timerBox.innerHTML = `<p>Computer Wins!</p>`;
   } else if (playerHand === 'rock' && computerHand === 'scissors') {
-    timerBox.innerHTML = `<p>Player Wins!</p>`;
+    timerBox.innerHTML = `<p>${playerName} Wins!</p>`;
   } else if (playerHand === 'paper' && computerHand === 'rock') {
-    timerBox.innerHTML = `<p>Player Wins!</p>`;
+    timerBox.innerHTML = `<p>${playerName} Wins!</p>`;
   } else if (playerHand === 'paper' && computerHand === 'scissors') {
     timerBox.innerHTML = `<p>Computer Wins!</p>`;
   } else if (playerHand === 'scissors' && computerHand === 'rock') {
     timerBox.innerHTML = `<p>Computer Wins!</p>`;
   } else if (playerHand === 'scissors' && computerHand === 'paper') {
-    timerBox.innerHTML = `<p>Player Wins!</p>`;
+    timerBox.innerHTML = `<p>${playerName} Wins!</p>`;
   }
 
-  if (timerBox.innerHTML === `<p>Player Wins!</p>`) {
+  if (timerBox.innerHTML === `<p>${playerName} Wins!</p>`) {
     playerScore++;
   } else if (timerBox.innerHTML === `<p>Computer Wins!</p>`) {
     computerScore++;
